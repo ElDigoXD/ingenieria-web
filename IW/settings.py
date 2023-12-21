05d9dd86d6c4d2f81321497807630bac220dac7e
Diego Sanz <digosanf@gmail.com>
2023-12-21 16:17:09 +0100

    Soporte de traducción estática

--- IW/settings.py
+++ IW/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_htmx',
+    'rosetta',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -107,11 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
@@ -126,3 +129,17 @@ STATICFILES_DIRS = [BASE_DIR / 'IW' / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Translations
+# https://testdriven.io/blog/multiple-languages-in-django/
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES = (
+    ('es', _('Español')),
+    ('en', _('Inglés')),
+)
+
+LOCALE_PATHS = [
+    BASE_DIR / 'IW' / 'locale/',
+]
--- IW/urls.py
+++ IW/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
 from .views import *
 
-urlpatterns = [
-    path('', index),
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-]
+    path('rosetta/', include('rosetta.urls')),
+    path('', index),
+)
