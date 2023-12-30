a6c24513f25a179d4724f1630656498988595b81
Diego Sanz <digosanf@gmail.com>
2023-12-30 13:13:55 +0100

    Permitir todos los hosts

--- IW/settings.py
+++ IW/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9yj_*!#t$ker%l8wb5ab5lh&%1m+1so$i21y5&*9kuzc8(hv&x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
